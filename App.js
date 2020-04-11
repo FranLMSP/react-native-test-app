83a2eebd22af67ace928712a01a24840168187cf
Franco Colmenarez <francoacg1@gmail.com>
2020-04-11 16:02:52 -0500

    Items can be deleted

--- App.js
+++ App.js
@@ -24,6 +24,12 @@ export default function App() {
     )
   }
 
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter(goal => goal.uid != goalId)
+    })
+  }
+
   return (
     <View style={ styles.screen }>
       <GoalInput onAddGoal={ addGoalHandler } />
@@ -31,7 +37,7 @@ export default function App() {
       <FlatList
         keyExtractor={ (item, index) => item.uid }
         data={ courseGoals }
-        renderItem={ itemData => <GoalItem title={ itemData.item.value } />}
+        renderItem={ itemData => <GoalItem id={ itemData.item.uid } onDelete={ removeGoalHandler } title={ itemData.item.value } />}
       />
     </View>
   );
